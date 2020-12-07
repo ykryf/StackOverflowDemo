d4bb228bd9929f4f559d0092008af06824e0d125
Yiannis Kryfos <yianniskryfos@yahoo.com>
2020-12-07 19:38:18 +0200

    Added EF Core and scaffolded from DB Classes.

---
+++ StackOverflowDemo.Api/Data/LinkType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StackOverflowDemo.Api.Data
+{
+    public partial class LinkType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ StackOverflowDemo.Api/Data/StackOverflow2013Context.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace StackOverflowDemo.Api.Data
+{
+    public partial class StackOverflow2013Context : DbContext
+    {
+        public StackOverflow2013Context()
+        {
+        }
+
+        public StackOverflow2013Context(DbContextOptions<StackOverflow2013Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Badge> Badges { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
+        public virtual DbSet<LinkType> LinkTypes { get; set; }
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<PostLink> PostLinks { get; set; }
+        public virtual DbSet<PostType> PostTypes { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<Vote> Votes { get; set; }
+        public virtual DbSet<VoteType> VoteTypes { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Badge>(entity =>
+            {
+                entity.Property(e => e.Date).HasColumnType("datetime");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(40);
+            });
+
+            modelBuilder.Entity<Comment>(entity =>
+            {
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Text)
+                    .IsRequired()
+                    .HasMaxLength(700);
+            });
+
+            modelBuilder.Entity<LinkType>(entity =>
+            {
+                entity.Property(e => e.Type)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Post>(entity =>
+            {
+                entity.Property(e => e.Body).IsRequired();
+
+                entity.Property(e => e.ClosedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.CommunityOwnedDate).HasColumnType("datetime");
+
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+
+                entity.Property(e => e.LastActivityDate).HasColumnType("datetime");
+
+                entity.Property(e => e.LastEditDate).HasColumnType("datetime");
+
+                entity.Property(e => e.LastEditorDisplayName).HasMaxLength(40);
+
+                entity.Property(e => e.Tags).HasMaxLength(150);
+
+                entity.Property(e => e.Title).HasMaxLength(250);
+            });
+
+            modelBuilder.Entity<PostLink>(entity =>
+            {
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+            });
+
+            modelBuilder.Entity<PostType>(entity =>
+            {
+                entity.Property(e => e.Type)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+
+                entity.Property(e => e.DisplayName)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.EmailHash).HasMaxLength(40);
+
+                entity.Property(e => e.LastAccessDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Location).HasMaxLength(100);
+
+                entity.Property(e => e.WebsiteUrl).HasMaxLength(200);
+            });
+
+            modelBuilder.Entity<Vote>(entity =>
+            {
+                entity.Property(e => e.CreationDate).HasColumnType("datetime");
+            });
+
+            modelBuilder.Entity<VoteType>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
