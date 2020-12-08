06d2cd7fdeb482407a36f8808b26843dd5ea1bd6
Yiannis Kryfos <yianniskryfos@yahoo.com>
2020-12-08 19:17:54 +0200

    Added EF Core, connection string and extension method for connections string.

---
+++ StackOverflowDemo.Api/Extensions/ExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackOverflowDemo.Api.Extensions
+{
+    public static class ExtensionMethods
+    {
+        public static string  GetConnectionStringUsingSecrets(this IConfiguration config, string connectionStringName, bool trusted = true)
+        {
+            string connectionString = config.GetConnectionString(connectionStringName);
+            var secrets = config.GetSection(connectionStringName);
+            string server = $"Server={secrets["Server"]};";
+            if (trusted)
+            {
+                return server + connectionString;
+            }
+            string user = $"UserId={secrets["User"]};";
+            string password = $"Password={secrets["Password"]}";
+            return server + user + password + connectionString;
+        }
+    }
+}
