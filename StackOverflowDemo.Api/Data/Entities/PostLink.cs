e6be106ca7f783bec793b333ef0276d10aafd65f
Yiannis Kryfos <yianniskryfos@yahoo.com>
2020-12-08 19:16:18 +0200

    Changed Namespace to entity models.

---
+++ StackOverflowDemo.Api/Data/Entities/PostLink.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StackOverflowDemo.Api.Data.Entities
+{
+    public partial class PostLink
+    {
+        public int Id { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int PostId { get; set; }
+        public int RelatedPostId { get; set; }
+        public int LinkTypeId { get; set; }
+    }
+}
---
+++ StackOverflowDemo.Api/Data/Entities/PostType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace StackOverflowDemo.Api.Data.Entities
+{
+    public partial class PostType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
